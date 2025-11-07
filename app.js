ae4e0eff7f96f2972ec385d6b826e4def7270993
Wixda <Iamwixda@gmail.com>
2025-11-07 17:58:52 +0100

    design the calculator app for theme 1

---
+++ app.js
@@ -0,0 +1,24 @@
+
+let operand1
+let operator
+let operand2
+
+function add (num1, num2) {
+    return num1 + num2;
+}
+
+function substract (num1, num2) {
+    return num1 - num2;
+}
+
+function multiply (num1, num2) {
+    return num1 * num2;
+}
+
+function divide (num1, num2) {
+    return num1 / num2;
+}
+
+function operate (operator, operand1, operand2) {
+    
+}
