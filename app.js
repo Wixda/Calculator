42ff10afa66a35e2b8f9c87df17b42be231e984a
Wixda <Iamwixda@gmail.com>
2025-11-12 23:33:14 +0100

    fix a lot of design issues

--- app.js
+++ app.js
@@ -1,3 +1,22 @@
+const calculatorDisplay = document.querySelector('#calculator-display');
+const oneBtn = document.querySelector('#one-btn');
+const twoBtn = document.querySelector('#two-btn');
+const threeBtn = document.querySelector('#three-btn');
+const fourBtn = document.querySelector('#four-btn');
+const fiveBtn = document.querySelector('#five-btn');
+const sixBtn = document.querySelector('#six-btn');
+const sevenBtn = document.querySelector('#seven-btn');
+const eightBtn = document.querySelector('#eight-btn');
+const nineBtn = document.querySelector('#nine-btn');
+const allDigitsBtns = document.querySelectorAll('.digit-btn');
+const decimalBtn = document.querySelector('#decimal-btn');
+const minusBtn = document.querySelector('#minus-btn');
+const divideBtn = document.querySelector('#divide-btn');
+const multiplyBtn = document.querySelector('#multiply-btn');
+const plusBtn = document.querySelector('#plus-btn');
+const operatorBtn = document.querySelector('#operator-btn');
+const delBtn = document.querySelector('#delBtn');
+const resetBtn = document.querySelector('#operator-btn');
 
 let operand1
 let operator
@@ -20,5 +39,22 @@ function divide (num1, num2) {
 }
 
 function operate (operator, operand1, operand2) {
-    
+    if (operator === "+") {
+        add (operand1, operand2);
+    } else if(operator === "-") {
+        substract (operand1, operand2);
+    } else if (operator === "*") {
+        multiply (operand1, operand2);
+    } else if (operator === "/") {
+        divide (operand1, operand2);
+    } else return null;
 }
+
+allDigitsBtns.forEach( button => {
+    button.addEventListener('click', () => {
+        const clickedButton = event.target;
+        calculatorDisplay.value += clickedButton.textContent;
+        const currentValue = calculatorDisplay.value;
+        console.log(currentValue);
+    })
+})
